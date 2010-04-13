5e802d064fe9d76175b0f51d6a3f0f017cd4c6da
Mike Mazur <mmazur@gmail.com>
2010-04-13 20:29:17 +0800

    Use format, not string, print raw instruction

--- src/bound_variable/decompile.clj
+++ src/bound_variable/decompile.clj
@@ -8,59 +8,59 @@
 
 (defmethod decompile-instruction 0x0 [instruction]
   (let [[ra rb rc] (get-standard-registers instruction)]
-    (str "cmv r" ra ", r" rb ", r" rc)))
+    (format "0x%08x cmv r%d, r%d, r%d" instruction ra rb rc)))
 
 (defmethod decompile-instruction 0x1 [instruction]
   (let [[ra rb rc] (get-standard-registers instruction)]
-    (str "aix r" ra ", r" rb ", r" rc)))
+    (format "0x%08x aix r%d, r%d, r%d" instruction ra rb rc)))
 
 (defmethod decompile-instruction 0x2 [instruction]
   (let [[ra rb rc] (get-standard-registers instruction)]
-    (str "arm r" ra ", r" rb ", r" rc)))
+    (format "0x%08x arm r%d, r%d, r%d" instruction ra rb rc)))
 
 (defmethod decompile-instruction 0x3 [instruction]
   (let [[ra rb rc] (get-standard-registers instruction)]
-    (str "add r" ra ", r" rb ", r" rc)))
+    (format "0x%08x add r%d, r%d, r%d" instruction ra rb rc)))
 
 (defmethod decompile-instruction 0x4 [instruction]
   (let [[ra rb rc] (get-standard-registers instruction)]
-    (str "mul r" ra ", r" rb ", r" rc)))
+    (format "0x%08x mul r%d, r%d, r%d" instruction ra rb rc)))
 
 (defmethod decompile-instruction 0x5 [instruction]
   (let [[ra rb rc] (get-standard-registers instruction)]
-    (str "div r" ra ", r" rb ", r" rc)))
+    (format "0x%08x div r%d, r%d, r%d" instruction ra rb rc)))
 
 (defmethod decompile-instruction 0x6 [instruction]
   (let [[ra rb rc] (get-standard-registers instruction)]
-    (str "nta r" ra ", r" rb ", r" rc)))
+    (format "0x%08x nta r%d, r%d, r%d" instruction ra rb rc)))
 
 (defmethod decompile-instruction 0x7 [instruction]
-  (str "hlt"))
+  (format "0x%08x hlt" instruction))
 
 (defmethod decompile-instruction 0x8 [instruction]
   (let [[ra rb rc] (get-standard-registers instruction)]
-    (str "alc r" rb ", r" rc)))
+    (format "0x%08x alc r%d, r%d" instruction rb rc)))
 
 (defmethod decompile-instruction 0x9 [instruction]
   (let [[ra rb rc] (get-standard-registers instruction)]
-    (str "abd r" rc)))
+    (format "0x%08x abd r%d" instruction rc)))
 
 (defmethod decompile-instruction 0xa [instruction]
   (let [[ra rb rc] (get-standard-registers instruction)]
-    (str "out r" rc)))
+    (format "0x%08x out r%d" instruction rc)))
 
 (defmethod decompile-instruction 0xb [instruction]
   (let [[ra rb rc] (get-standard-registers instruction)]
-    (str "ipt r" rc)))
+    (format "0x%08x ipt r%d" instruction rc)))
 
 (defmethod decompile-instruction 0xc [instruction]
   (let [[ra rb rc] (get-standard-registers instruction)]
-    (str "ldp r" rb ", r" rc)))
+    (format "0x%08x ldp r%d, r%d" instruction rb rc)))
 
 (defmethod decompile-instruction 0xd [instruction]
   (let [ra (bit-and (bit-shift-right instruction *load-register-offset*) *register-mask*)
         value (bit-and instruction *load-value-mask*)]
-    (str "lod r" ra ", " value)))
+    (format "0x%08x lod r%d, %d" instruction ra value)))
 
 (defmethod decompile-instruction :default [instruction]
-  (format "unknown 0x%h" instruction))
+  (format "unknown 0x%08x" instruction))
